33a0cb1524b51dcd73b57daf08adbff4fbe8b860
riley soloner <rileysoloner@gmail.com>
2022-08-12 16:51:49 -0400

    commit tree ceeac8d88268ff7bee13eee76ff04364735fbb3e

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def condition=(c)
+    @condition = c
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+  
+end
